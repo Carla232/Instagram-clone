c728102c690518f83d82a223c08c3cde94bba533
Carla232 <79741526+Carla232@users.noreply.github.com>
2022-02-26 00:23:39 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+// TODO:  add to tailwind config
+// bg-blue-medium -> hex values
+// text-red-primary -> hex values
+// text-blue-medium -> hex values
+// text-gray-base -> hex values
+// text-gray-primary -> hex values
+
+module.exports = {
+    future: {
+      removeDeprecatedGapUtilities: true
+    },
+    purge: {
+      enabled: true,
+      content: ['./src/**/*.js', './src/**/**/*.js']
+    },
+    theme: {
+      fill: (theme) => ({
+        red: theme('colors.red.primary')
+      }),
+      colors: {
+        white: '#ffffff',
+        blue: {
+          medium: '#005c98'
+        },
+        black: {
+          light: '#262626',
+          faded: '#00000059'
+        },
+        gray: {
+          base: '#616161',
+          background: '#fafafa',
+          primary: '#dbdbdb'
+        },
+        red: {
+          primary: '#ed4956'
+        }
+      }
+    },
+    variants: {
+      extend: {
+        display: ['group-hover']
+      }
+    }
+  };
+  
